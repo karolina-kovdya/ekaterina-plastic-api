ff868be508ad309ffa1840bd9059736193e492f6
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2023-08-08 21:34:49 +0300

    устанавливает пакет cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const helmet = require('helmet')
 const routes = require('./routes/user');
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
+app.use(cors())
 app.use(routes)
 
 const { PORT = 3001 } = process.env;
